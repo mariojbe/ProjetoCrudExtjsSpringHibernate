12b975d3853d5150a45cf34076e7a7a132035dd0
mariojbe <mariojbe@gmail.com>
2015-11-06 13:43:41 -0300

    Adicionando ExtJS

---
+++ src/main/webapp/app/store/Contatos.js
@@ -0,0 +1,27 @@
+Ext.define('ExtMVC.store.Contatos', {
+    extend: 'Ext.data.Store',
+    model: 'ExtMVC.model.Contato',
+    autoLoad: true,
+    pageSize: 35,
+    autoLoad: {start: 0, limit: 35},
+    proxy: {
+        type: 'ajax',
+        api: {
+            create: 'contact/create.action',
+            read: 'contact/view.action',
+            update: 'contact/update.action',
+            destroy: 'contact/delete.action',
+        },
+        reader: {
+            type: 'json',
+            root: 'contatos',
+            successProperty: 'success'
+        },
+        writer: {
+            type: 'json',
+            writeAllFields: true,
+            encode: true,
+            root: 'contatos'
+        }
+    }
+});
